53bd500cef30ffbe10a0f9c036cc7eab6fb3a6bd
Fazley <22FTT1494@student.pb.edu.bn>
2024-04-08 09:31:08 +0800

    Update App.vue to add a heading element and improve readability

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
