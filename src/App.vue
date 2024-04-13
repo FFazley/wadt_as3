7720261d445c0fdb38cbb43ceefa6915b7aeca66
Fazley <22FTT1494@student.pb.edu.bn>
2024-04-13 23:50:09 +0800

    Update App.vue to improve profile information and styling

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,32 @@ import profilePic from './assets/download.jpg';
 
 <template>
   <div class="p-6 sm:p-12 md:p-24 flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-500 to-indigo-500 rounded">
-    <img :src="profilePic" alt="Profile Picture" class="w-32 h-32 rounded-full mb-4 shadow-lg" />
+    <img :src="profilePic" alt="Profile Picture" class="w-50 h-50 rounded-full mb-4 shadow-lg border-2 border-black" />
     <div class="text-center" style="font-family: Helvetica;">
       <h1 class="text-3xl font-bold text-white mb-4">Md Farmiziee Fazley</h1>
       <p class="text-lg text-gray-200">
-        My student id is 22FTT1494 taking course WEB DEVELOPMENT. I like to explore the world of technology, from coding in Python to designing creative projects with Scratch. I have a passion for problem-solving and love learning new skills like web development and graphic design. Outside of school, I enjoy playing sports, volunteering in my community, and spending time with friends.
-      </p>
-      <h2 class="text-xl font-semibold text-white mt-8">Skills</h2>
-      <ul class="text-lg text-gray-200">
+        My student id is 22FTT1494 taking course WEB DEVELOPMENT. </p>
+      <div class="flex justify-between">
+        <div>
+          <h2 class="text-xl font-semibold text-white mt-8">Skills</h2>
+          <ul class="text-lg text-gray-200">
         <li>HTML</li>
         <li>CSS</li>
         <li>JavaScript</li>
         <li>Vue.js</li>
         <li>Python</li>
-      </ul>
+          </ul>
+        </div>
+        <div>
+          <h2 class="text-xl font-semibold text-white mt-8">Hobbies</h2>
+          <ul class="text-lg text-gray-200">
+        <li>Reading</li>
+        <li>Playing guitar</li>
+        <li>Hiking</li>
+        <li>Photography</li>
+          </ul>
+        </div>
+      </div>
       <h2 class="text-xl font-semibold text-white mt-8">Contact</h2>
       <ul class="text-lg text-gray-200">
         <li>Email: example@example.com</li>
