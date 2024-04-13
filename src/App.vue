b1fda936534ef9fd9327adb401a6de5a5e34e389
Fazley <22FTT1494@student.pb.edu.bn>
2024-04-13 22:41:33 +0800

    Update App.vue to include profile picture and personal information

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 
-
+import profilePic from './assets/download.jpg';
 </script>
 
 <template>
-  
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="p-6 sm:p-12 md:p-24 flex flex-col items-center justify-center min-h-screen bg-gray-500">
+    <img :src="profilePic" alt="Profile Picture" class="w-32 h-32 rounded-full mb-4 shadow-lg" />
+    <p class="text-black-600 text-center">
+      My name is Md Farmiziee Fazley. my student id is  22FTT1494 taking course WEB DEVELOPMENT. I like to exploring the world of technology, from coding in Python to designing creative projects with Scratch. I have a passion for problem-solving and love learning new skills like web development and graphic design. Outside of school, I enjoy playing sports, volunteering in my community, and spending time with friends.
+        </p>
+  </div>
 </template>
 
 <style>
-
 </style>
